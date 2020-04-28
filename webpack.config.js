a875a6248f79478623d6a4b09ab415b4bf6ffdc6
Bikal Gurung <gbikal@gmail.com>
2020-04-29 00:38:39 +0100

    Add html-webpack plugin to automate html

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
-const path = require('path');
+// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     virtualDom: './src/virtualDom.js',
-    
+    // virtualDom2: './src/virtualDom2.js'
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "virtual-dom - 2.2.1",
+      filename: "virtualDom.html",
+      chunks: ['virtualDom']
+    }),
+    // new HtmlWebpackPlugin({
+    //   title: "virtual-dom - 2.2.1",
+    //   filename: "virtualDom2.html",
+    //   chunks : ['virtualDom2']
+    // }),
+  ]
 };
